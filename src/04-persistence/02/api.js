f5ce3d22e273fcf6d92bc0298a1d758a0a83d569
jonathan_fambro <startek591@gmail.com>
2025-03-28 16:35:59 -0400

    More Validations

---
+++ src/04-persistence/02/api.js
@@ -0,0 +1,48 @@
+const Products = require('./products');
+const autoCatch = require('./lib/auto-catch');
+
+module.exports = autoCatch({
+  getProduct,
+  listProducts,
+  createProduct,
+  editProduct,
+  deleteProduct,
+});
+
+async function getProduct(req, res, next) {
+  const { id } = req.params;
+
+  const product = await Products.get(id);
+  if (!product) return next();
+
+  res.json(product);
+}
+
+async function listProducts(req, res, next) {
+  const { offset = 0, limit = 25, tag } = req.query;
+
+  const products = await Products.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    tag,
+  });
+
+  res.json(products);
+}
+
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body);
+  res.json(product);
+}
+
+async function editProduct(req, res, next) {
+  const change = req.body;
+  const product = await Products.edit(req.params.id, change);
+
+  res.json(product);
+}
+
+async function deleteProduct(req, res, next) {
+  await Products.remove(req.params.id);
+  res.json({ success: true });
+}
